0919cd0ef168b4e0e7e0af0438d083043f6f8cf4
Ronald Kwan <ronaldkwan93@gmail.com>
2024-04-09 20:47:12 +1000

    npm setup

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
+console.log(process.env.ENVIRONMENT_FILE)
+
 console.log("Starting!")
 
 const pokemon = require('pokemon');
 
 let randomPokemon = pokemon.random();
 
-console.log(`Today's Pokemon is ${randomPokemon}`)
+console.log(`Today's favourite Pokemon is ${randomPokemon}`)
